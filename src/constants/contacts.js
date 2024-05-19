bbb19c8233f83449be4768a48f5239ebfc7607cd
Apaccer <142975619+Apaccer@users.noreply.github.com>
2024-05-19 15:58:02 +0300

    add_generateContacts

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,3 @@
-export const PATH_DB =
+import fs from 'node:fs/promises';
+
+export const PATH_DB = 'src/db/db.json';
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,30 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
+const generateContacts = async (number) => {
+  const createFakeContacts = (number) => {
+    const contacts = [];
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+    return contacts;
+  };
+  try {
+    const oldContacts = await fs.readFile(PATH_DB, 'utf8');
+    const parseOldContacts = JSON.parse(oldContacts);
 
-const generateContacts = async (number) => {};
+    const newContacts = await createFakeContacts(number);
+    console.log(newContacts);
+    const updatedContacts = [...parseOldContacts, ...newContacts];
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf8',
+    );
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 await generateContacts(5);
